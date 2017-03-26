86d030085e4e8c9c56fff70d6022d2f11e7313ec
Jonathan Ingram <Jonathan Ingram>
2017-03-26 10:13:35 -0500

    Initial commit

---
+++ app/src/main/java/net/ingramintegrations/sendemail/MainActivity.java
@@ -0,0 +1,54 @@
+package net.ingramintegrations.sendemail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText recepient, subject, body;
+
+    String emailAddress, emailSubject, emailBody;
+    Button sendEmailButton;
+
+    Intent SendEmailIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recepient = (EditText) findViewById(R.id.act_main_et_email_address);
+        subject = (EditText) findViewById(R.id.act_main_et_email_subject);
+        body = (EditText) findViewById(R.id.act_main_et_email_body);
+        sendEmailButton = (Button) findViewById(R.id.act_main_bt_send_email_button);
+
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                emailAddress = recepient.getText().toString();
+                emailSubject = subject.getText().toString();
+                emailBody = body.getText().toString();
+
+                SendEmailIntent = new Intent(Intent.ACTION_SEND);
+                SendEmailIntent.setType("message/rfc822");
+                SendEmailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailAddress});
+                SendEmailIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
+                SendEmailIntent.putExtra(Intent.EXTRA_TEXT, emailBody);
+
+                try {
+                    startActivity(Intent.createChooser(SendEmailIntent, "Send email..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getApplicationContext(), "There are no email clients installed.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+}
